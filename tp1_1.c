ffecc9a775a26683e1b9b8779ef7d80f41f0c8a0
Tadeo Munoz <tadeomunoz09@hotmail.com>
2024-04-03 20:42:07 -0300

    termine el punto 1

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
-//a
+// a
 
-int main(){
-    printf("Hola mundo \n");
+int main() {
+    // printf("Hola mundo \n");
+    // e
+    int variable = 34;
+    int *puntero = &variable;
+    printf("Contenido del puntero: %d \n", *puntero);
+    printf("Direccion de memoria almacenada por el puntero: %p \n", puntero);
+    printf("Direccion de memoria de la variable: %p \n", &variable);
+    printf("Direccion de memoria del puntero: %p \n", &puntero);
+    printf("Tamanio de memoria de la variable: %zu \n", sizeof(variable));
+
     return 0;
 }
